e333892968dde9542a3ae6b19cedbee3b7db7278
b3tt3rfun <37350140+b3tt3rfun@users.noreply.github.com>
2018-03-16 21:17:28 +0100

    Response header "X-Ufl-Realserver" changed by "Date"
    This header has not always the same name. So changing it by another one.

--- res-header-bug.js
+++ res-header-bug.js
@@ -5,10 +5,10 @@ function onResponse(req, res){
   log();
   if(res.ContentType){
     if (res.ContentType.indexOf("text/html") == 0){
-      if (res.Headers.indexOf("X-Ufl-Realserver") != -1){
-        var tmp1 = res.Headers.split("X-Ufl-Realserver: ");
+      if (res.Headers.indexOf("Date") != -1){
+        var tmp1 = res.Headers.split("Date: ");
         var tmp2 = tmp1[1].split("\r\n");
-        res.Headers = tmp1[0] + "X-Ufl-Realserver: 127.0.0.1\r\n" + tmp2[1];
+        res.Headers = tmp1[0] + "Date: test\r\n" + tmp2[1];
         //res.ReadBody();  // Needed to setup len(jres.Body) != 0
       }
     }
