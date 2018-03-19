8659e543c10022fa56dc15968f1c324c3c92194a
b3tt3rfun <37350140+b3tt3rfun@users.noreply.github.com>
2018-03-19 16:47:42 +0100

    Code is buggy and only next header is appended
    As pointed out by @yungtravla this code is faulty and only next header is appended. I should use string.replace but i like the risk ;-)

--- res-header-bug.js
+++ res-header-bug.js
@@ -6,9 +6,14 @@ function onResponse(req, res){
   if(res.ContentType){
     if (res.ContentType.indexOf("text/html") == 0){
       if (res.Headers.indexOf("Date") != -1){
+        log("Headers before: " + res.Headers);
         var tmp1 = res.Headers.split("Date: ");
         var tmp2 = tmp1[1].split("\r\n");
-        res.Headers = tmp1[0] + "Date: test\r\n" + tmp2[1];
+        res.Headers = tmp1[0] + "Date: 127.0.0.1\r\n";
+        for(var i=1;i<tmp2.length;i++){
+          res.Headers += tmp2[i] + "\r\n";
+        }
+        log("Headers after: " + res.Headers);
         //res.ReadBody();  // Needed to setup len(jres.Body) != 0
       }
     }
